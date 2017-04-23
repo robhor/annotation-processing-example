703a9f1cb2a40cc0c5bade5c7d7fafebd863eafd
robhor <robhor@gmail.com>
2017-04-23 15:54:39 +0200

    Initial commit

---
+++ app/src/main/java/at/robhor/annotationprocessingexample/MainActivity.java
@@ -0,0 +1,13 @@
+package at.robhor.annotationprocessingexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
