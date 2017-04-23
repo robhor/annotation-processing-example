dd39964db0e751769a72cf17b2cc038f1d774a53
robhor <robhor@gmail.com>
2017-04-23 16:00:36 +0200

    Add annotation-compiler module

---
+++ annotation-compiler/src/main/java/at/robhor/annotationprocessingexample/BindViewProcessor.java
@@ -0,0 +1,26 @@
+package at.robhor.annotationprocessingexample;
+
+import java.util.Collections;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+
+public class BindViewProcessor extends AbstractProcessor {
+    @Override
+    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+        return false;
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return Collections.singleton(BindView.class.getCanonicalName());
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+}
