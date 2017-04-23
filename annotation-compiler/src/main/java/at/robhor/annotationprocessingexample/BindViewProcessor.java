1e0f5295150c37e2ef75f12cacd1196c43129ab2
robhor <robhor@gmail.com>
2017-04-23 16:08:29 +0200

    Register annotation processor

--- annotation-compiler/src/main/java/at/robhor/annotationprocessingexample/BindViewProcessor.java
+++ annotation-compiler/src/main/java/at/robhor/annotationprocessingexample/BindViewProcessor.java
@@ -4,13 +4,18 @@ import java.util.Collections;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
 
 public class BindViewProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+        Messager messager = processingEnv.getMessager();
+        messager.printMessage(Diagnostic.Kind.NOTE, "BindViewProcessor is running");
+
         return false;
     }
 
