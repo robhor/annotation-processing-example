955aa25d2c42ebb132f042dcdbd2f4f67249b7b3
robhor <robhor@gmail.com>
2017-04-23 15:56:35 +0200

    Customize layout

--- app/src/main/java/at/robhor/annotationprocessingexample/MainActivity.java
+++ app/src/main/java/at/robhor/annotationprocessingexample/MainActivity.java
@@ -2,12 +2,22 @@ package at.robhor.annotationprocessingexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView titleView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        titleView = (TextView) findViewById(R.id.title);
+        textView = (TextView) findViewById(R.id.text);
+
+        titleView.setText("Annotation Processing Example");
+        textView.setText("The old way - calling findViewById manually");
     }
 }
